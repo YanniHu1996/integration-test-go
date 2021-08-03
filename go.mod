20007ce39abdc68e4c21e01fd96546d1bfeb6730
YanniHu <1043101807@qq.com>
2021-08-03 23:41:25 +0800

    site

--- go.mod
+++
@@ -1,5 +0,0 @@
-module integration-test
-
-go 1.16
-
-require github.com/smartystreets/goconvey v1.6.4
--- main.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func main() {
-	// https://github.com/YanniHu1996/integration-test-go.wiki.git
-}
